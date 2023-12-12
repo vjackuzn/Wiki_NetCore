c91b46ef1fa84cff0d6b56ec754a2fb6a27e1fee
vjackuzn <vjackuzn@gmail.com>
2023-12-12 14:25:10 +0200

    added article Model

---
+++ Backend_NET/Models/Article.cs
@@ -0,0 +1,10 @@
+namespace Backend_NET.Models
+{
+    public partial class Article
+    {
+        public int ArticleId { get; set; }
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public DateTime CreatedAt { get; set; } = DateTime.Today;
+    }
+}
