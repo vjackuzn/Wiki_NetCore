4f8abd0e07320587d2f4962793f699e3ddff99ae
vjackuzn <vjackuzn@gmail.com>
2023-12-13 19:01:54 +0200

    added empty template for ArticleList component

--- Frontend_Angular/src/app/app.routes.ts
+++ Frontend_Angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ArticleListComponent } from './features/articles/article-list/article-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ArticleListComponent
+    }
+];
