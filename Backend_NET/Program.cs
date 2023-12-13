4f2a1b1fb8a93f50e7d5bfeb37b32fabae98a06e
vjackuzn <vjackuzn@gmail.com>
2023-12-13 19:18:35 +0200

    addd CORS policies

--- Backend_NET/Program.cs
+++ Backend_NET/Program.cs
@@ -7,16 +7,37 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors((options) => 
+    {
+        options.AddPolicy("DevCors", (corsBuilder) => 
+            {
+                corsBuilder.WithOrigins("http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+
+        options.AddPolicy("ProdCors", (corsBuilder) => 
+            {
+                corsBuilder.WithOrigins("https://myProductionSite.com")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("DevCors");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 else
 {
+    app.UseCors("ProdCors");
     app.UseHttpsRedirection();
 }
 
