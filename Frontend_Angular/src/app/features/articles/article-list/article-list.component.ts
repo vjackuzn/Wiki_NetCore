9550794bb834c67a9e7d9fc14ebcda17229e3ff8
vjackuzn <vjackuzn@gmail.com>
2023-12-15 20:19:38 +0200

    added searching functionality

--- Frontend_Angular/src/app/features/articles/article-list/article-list.component.ts
+++ Frontend_Angular/src/app/features/articles/article-list/article-list.component.ts
@@ -4,26 +4,30 @@ import { Article } from '../models/article.model';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-article-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './article-list.component.html',
   styleUrl: './article-list.component.css',
   providers: [ArticleService],
 })
 export class ArticleListComponent implements OnInit, OnDestroy {
-  articles: Article[] | undefined;
+  articlesFromAPI?: Article[];
+  searchString: string = '';
+  articlesToDisplay?: Article[];
 
-  private articleListSubscription: Subscription | undefined;
+  private articleListSubscription?: Subscription;
 
   constructor(private articleService: ArticleService, private router: Router) {}
 
   ngOnInit(): void {
     this.articleService.getAllArticles().subscribe({
       next: (response) => {
-        this.articles = response;
+        this.articlesFromAPI = response;
+        this.articlesToDisplay = this.articlesFromAPI;
       },
       error: (err) => {
         console.error('Error fetching list of articles:', err);
@@ -37,5 +41,19 @@ export class ArticleListComponent implements OnInit, OnDestroy {
 
   onCardClick(article: Article): void {
     this.router.navigate(['/articles/list', article.articleId]);
+  }
+
+  onSearchChange(): void {
+    if (!this.articlesFromAPI) {
+      return;
+    }
+
+    this.articlesToDisplay = this.articlesFromAPI.filter(
+      (article) =>
+        article.title.toLowerCase().includes(this.searchString.toLowerCase()) ||
+        article.description
+          .toLowerCase()
+          .includes(this.searchString.toLowerCase())
+    );
   }
 }
