e43d99bba48291c8a047213aeabb5bc721294e6f
vjackuzn <vjackuzn@gmail.com>
2023-12-16 18:40:24 +0200

    added Template value to Article types

--- Frontend_Angular/src/app/shared/shared-constants.ts
+++ Frontend_Angular/src/app/shared/shared-constants.ts
@@ -1,4 +1,5 @@
 export const ARTICLE_TYPES = [
   { value: 'Support issue', label: 'Support issue' },
   { value: 'Technical issue', label: 'Technical issue' },
+  { value: 'Template', label: 'Template' },
 ];
